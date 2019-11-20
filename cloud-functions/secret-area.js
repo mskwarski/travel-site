ee8f431513c55165e3b5019b2b991ce62c80fccd
Marcin Skwarski <marcin.skwarski@nielsen.com>
2019-11-20 11:24:38 +0100

    secretContent

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,27 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Welcome to the sueper secret area"
-    })
+    const secretContent = 
+    <h3>Welcome to The Secret Area</h3>
+    <p>Quickly network proactive alignments <strong>whereas</strong> transparent results.</p>
+
+    
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.password == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: "Secret Content"
+        })
+    } else {
+        callback(null, {
+            statusCode: 401
+        })
+    }
+
+    
 }
