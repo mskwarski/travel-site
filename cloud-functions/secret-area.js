51ed76d378b3cb33b0c5083865b6f19cc6ac152f
Marcin Skwarski <marcin.skwarski@nielsen.com>
2019-11-20 11:33:01 +0100

    Updated secret-area.js

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,27 +1,27 @@
 exports.handler = function(event, context, callback) {
     const secretContent = `
-    <h3>Welcome to The Secret Area</h3>
-    <p>Quickly network proactive alignments <strong>whereas</strong> transparent results.</p>
+    <h3>Welcome To The Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
     `
     
     let body
-
+  
     if (event.body) {
-        body = JSON.parse(event.body)
+      body = JSON.parse(event.body)
     } else {
-        body = {}
+      body = {}
     }
-
+  
     if (body.password == "javascript") {
-        callback(null, {
-            statusCode: 200,
-            body: "Secret Content"
-        })
+      callback(null, {
+        statusCode: 200,
+        body: secretContent
+      })
     } else {
-        callback(null, {
-            statusCode: 401
-        })
+      callback(null, {
+        statusCode: 401
+      })
     }
-
+  
     
-}
+  }
