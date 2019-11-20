d56a63c6f25f33b5a3e8d738ab296d1d987b61cb
Marcin Skwarski <marcin.skwarski@nielsen.com>
2019-11-20 11:30:09 +0100

    2nd

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,8 +1,8 @@
 exports.handler = function(event, context, callback) {
-    const secretContent = 
+    const secretContent = `
     <h3>Welcome to The Secret Area</h3>
     <p>Quickly network proactive alignments <strong>whereas</strong> transparent results.</p>
-
+    `
     
     let body
 
